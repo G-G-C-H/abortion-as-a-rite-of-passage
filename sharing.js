12346ec0be405d13260f8e35a4ae6b8a76ceae7c
g <68551273+G-G-C-H@users.noreply.github.com>
2025-10-12 23:33:21 +0100

    Add files via upload

---
+++ sharing.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(function() {
+    $("#open-sesame").click(function() {
+      $("#first-container").toggle();
+    });
+  });
+
+  $(function() {
+    $("#text-two").click(function() {
+      $("#second-container").toggle();
+    });
+  });
+
+  $(function() {
+    $("#text-three").click(function() {
+      $("#third-container").toggle();
+    });
+  });
+});
